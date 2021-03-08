1dfab5efa4c0c856d71b04e03f007226468417b2
Xeukxz <ryandstanden@gmail.com>
2021-03-08 20:56:25 +0000

    t

--- public/index.js
+++ public/index.js
@@ -8,17 +8,22 @@ $(() => {
   let pannelNo
   let comms = false
   let userName
-  let mode = 'config'
+  let mode = 'Config'
   let args
-  let commandNames = ['setname']
+  let commandNames = ['setname', 'help']
+  let clientName = ''
 
   let error = 'red'
   let server = '#ff72ff'
+  let client = 'green'
 
   function clientSend(text, type) {
+    let sender
+    if(type == client) sender == clientName
+    else sender == 'Server'
     $('#holder').append(`
         <div class="pannel" id="pannel-${pannelNo}">
-          <p class="msg" style="color:${type}; margin-left: 5px;"> Server > ${text}</p>
+          <p class="msg" style="color:${type}; margin-left: 5px;"> ${sender} > ${text}</p>
         </div>`)
     pannelNo++
   }
@@ -30,33 +35,54 @@ $(() => {
 
 
 
-    if (event.keyCode == '13') {
+    if (event.keyCode == '13') { // if enter is pressed
 
-      args = $('#messageInput').val().split(' ')
+      args = $('#messageInput').val().split(' ') // set arguments
+      msg.content = $('#messageInput').val()
 
-      if (mode == 'config') {
-        if (commandNames.includes(args[0])) msg.type = 'command'
-        else clientSend('no', error)
+      if(msg.content == '' || msg.content == ' ') return
+
+      
+
+      if (mode == 'Config') {
+
+        if (commandNames.includes(args[0])) { // if first word is recognised command
+          msg.type = 'command'
+          console.log('hujdfhhjskdbf')
+        }
+
+        if (msg.type == 'command') {
+          if (msg.content == 'setname') {
+
+          } else if (msg.content == 'help') {
+            console.log('jaja')
+            clientSend('Available commands are: /help, /setname')
+          }
+        } else {
+          console.log('jajax')
+          clientSend(`Command '${args[0]}' not recognised`, error)
+        }
+
+        //else clientSend('no', error)
 
 
       }
 
 
-      if (val == '') return
+      /* if (val == '') return
       while (val.startsWith(' ')) val.replace(' ', '')
       if (val.startsWith('/')) msg.type = 'command'
-      else msg.type = 'message'
+      else msg.type = 'message' */
 
 
 
-      msg.content = $('#messageInput').val()
-      console.log(message)
+      console.log(msg.content)
 
       msg.color = msgColor
 
       $('#holder').append(`
         <div class="pannel" id="pannel-${pannelNo}">
-          <p class="msg" style="color:${msg.color};"> >${msg.content}</p>
+          <p class="msg" style="color:${msg.color};">  > ${msg.content}</p>
         </div>`)
 
       socket.emit('msg', { //sends message
